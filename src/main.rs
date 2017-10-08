1484ef2fa7455ef5b9981e91d34100d6e0b75371
taskie <t@skie.jp>
2017-10-08 22:47:44 +0900

    prepare Application class

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,98 @@
 extern crate sdl2;
-extern crate hlua;
 
-use hlua::Lua;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+struct Application {
+    sdl: Option<sdl2::Sdl>,
+    canvas: Option<sdl2::render::Canvas<sdl2::video::Window>>,
+    finished: bool,
+}
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 640, 480)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
+impl Application {
+    fn new() -> Application {
+        Application {
+            sdl: None,
+            canvas: None,
+            finished: false,
+        }
+    }
 
-    let mut lua = Lua::new();
-    lua.checked_set("x", 2).unwrap();
+    fn start(&mut self) {
+        self.prepare().run()
+    }
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    fn prepare(&mut self) -> &mut Self {
+        let sdl = sdl2::init().unwrap();
+        let video_subsystem = sdl.video().unwrap();
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+        let window = video_subsystem.window("rust-sdl2 demo: Video", 640, 480)
+            .position_centered()
+            .opengl()
+            .build()
+            .unwrap();
 
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
+        let mut canvas = window.into_canvas().build().unwrap();
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.present();
+
+        self.sdl = Some(sdl);
+        self.canvas = Some(canvas);
+        self
+    }
+
+    fn run(&mut self) {
+        let sdl = self.sdl.clone().unwrap();
+        let mut event_pump = sdl.event_pump().unwrap();
+
+        'running: loop {
+            let now = Instant::now();
+
+            for event in event_pump.poll_iter() {
+                self.process_event(event);
+            }
+            self.update();
+
+            if self.finished {
+                break 'running
+            }
+
+            let elapsed = now.elapsed();
+            let asleep_time = Duration::new(0, 1_000_000_000 / 60);
+            if let Some(nanos) = asleep_time.checked_sub(elapsed) {
+                ::std::thread::sleep(nanos);
+            } else {
+                ::std::thread::sleep(Duration::new(0, 0));
             }
         }
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-        // The rest of the game loop goes here...
     }
+
+
+    fn process_event(&mut self, event: Event) {
+        match event {
+            Event::Quit {..} => {
+                self.finished = true;
+            },
+            Event::KeyDown { keycode: Some(keycode), .. } => {
+
+            },
+            Event::KeyUp { keycode: Some(keycode), .. } => {
+
+            },
+            _ => {}
+        }
+    }
+
+    fn update(&mut self) {
+
+    }
+}
+
+pub fn main() {
+    let mut app = Application::new();
+    app.start()
+
 }
